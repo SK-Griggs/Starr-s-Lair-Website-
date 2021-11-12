62ad1d19621aec302fb074f31bbbc89526d7bf03
Starr K Griggs <starr.k.griggs@gmail.com>
2021-11-11 22:30:50 -0500

    email and validation

--- JS/java.js
+++ JS/java.js
@@ -7,3 +7,20 @@ function myFunction() {
     x.style.display = "none";
   }
 }
+
+
+function EmailValidation(enteredEmail)
+{var mail_format = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
+if(enteredEmail.value.match(enteredEmail))
+{alert(Thanks);
+document.form1.text1.focus();
+return true;
+}
+Else 
+{alert(Incorrect);
+document.form1.text1.focus();
+return false;
+}
+
+}
+
